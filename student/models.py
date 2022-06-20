581c7ffba75e5eb9a176606ce9d92279253e763a
fox3company <fox3company@gmail.com>
2022-06-20 17:57:32 +0300

    Created a model Lesson(v0.001)

--- student/models.py
+++ student/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
+
+
+class Lesson(models.Model):
+#General Info
+	name 		= models.CharField(max_length=35) 
+	about		= models.TextField()
+	meadia_repr = models.FileField(upload_to='lessons/')
+	#teacher		= models.ForeignKey('teacher.Teacher', on_delete=models.CASCADE)
+	quantity	= models.PositiveSmallIntegerField(default=25, validators=[MaxValueValidator(35)])
+
+#All information about, where lesson would be	
+	start_date  = models.DateField()
+	start_time  = models.TimeField() 
+	duration 	= models.TimeField()
+	#end_at 	= time
+
+#All information about, where lesson would be
+	loaction 	= models.URLField(default="https://goo.gl/maps/HZwWj15NgYrt2vBz9")
+	studios  	= [
+		(101,'Studio №=101'),
+		(109,'Studio №=109'),
+		(115,'Studio №=115'),
+		(201,'Studio №=201'),
+		(205,'Studio №=205'),
+		(208,'Studio №=208'),
+	]
+	studio_numb = models.IntegerField(choices=studios)
+	#approved = boolean
+
+
+	@property
+	def end_at(self):
+		timeList 	= [self.start_time, self.duration]
+		ending_time = datetime.timedelta() 
+		for i in timeList:
+		    (h, m, s) = (i.hour, i.minute, i.second)
+		    d = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+		    ending_time += d
+		return ending_time 
+
+
+	
