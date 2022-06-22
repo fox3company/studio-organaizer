0c45837e259b956b77c64c68a762d52706ebf51a
fox3company <fox3company@gmail.com>
2022-06-22 16:33:01 +0300

    Location, Studio, Student models have been added. The connection between them were established and corrected

--- student/admin.py
+++ student/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Lesson
+from .models import Lesson, Studio, Location
 
 # Register your models here.
 admin.site.register(Lesson)
+admin.site.register(Studio)
+admin.site.register(Location)
--- student/models.py
+++ student/models.py
@@ -1,36 +1,38 @@
 from django.db import models
-from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator,MaxValueValidator
+from django.core.validators import RegexValidator
 
-# Create your models here.
 
+class Location(models.Model):
+	name	= models.CharField(max_length=35, default="The Space project[Center]")
+	geo 	= models.URLField(default="https://goo.gl/maps/HZwWj15NgYrt2vBz9")
+
+	def __str__(self):
+		return self.name
+
+class Studio(models.Model):
+	number 	     = models.PositiveSmallIntegerField(validators=[MinValueValidator(1)], default=8)
+	geo_location = models.ForeignKey('Location', on_delete=models.CASCADE)
+
+	def __str__(self):
+		return str(self.number)
 
 class Lesson(models.Model):
 #General Info
 	name 		= models.CharField(max_length=35) 
 	about		= models.TextField()
 	meadia_repr = models.ImageField(upload_to='lessons/',height_field=None, width_field=None)
-	#teacher		= models.ForeignKey('teacher.Teacher', on_delete=models.CASCADE)
-	quantity	= models.PositiveSmallIntegerField(default=25, validators=[MaxValueValidator(35)])
+	teacher		= models.ForeignKey('teacher.Teacher', on_delete=models.CASCADE)
+	quantity	= models.PositiveSmallIntegerField(default=25, validators=[MaxValueValidator(35)]) #For that specifix version, iw would be no more than 35 people per room
 
-#All information about, where lesson would be	
+#All information about, when lesson would be	
 	start_date  = models.DateField()
 	start_time  = models.TimeField() 
 	duration 	= models.TimeField()
 	#end_at 	= time //For that version, ending is always on the same day. The studio doesn't support long events yet.
-
-#All information about, where lesson would be
-	loaction 	= models.URLField(default="https://goo.gl/maps/HZwWj15NgYrt2vBz9")
-	studios  	= [
-		(101,'Studio №=101'),
-		(109,'Studio №=109'),
-		(115,'Studio №=115'),
-		(201,'Studio №=201'),
-		(205,'Studio №=205'),
-		(208,'Studio №=208'),
-	]
-	studio_numb = models.IntegerField(choices=studios)
+# Where, lesson would be
+	studio  	= models.ForeignKey('Studio',on_delete=models.CASCADE)
 	#approved = boolean
-
 
 	@property
 	def end_at(self):
@@ -46,7 +48,15 @@ class Lesson(models.Model):
 		return self.name
 
 
+class Student(models.Model):
+	name 	 	 = models.CharField(max_length=35)
+	about	 	 = models.TextField(default='')
+	avatar   	 = models.ImageField(blank=True, upload_to='students/', height_field=None, width_field=None)
+	phone_regex  = RegexValidator(regex=r'^0?\d{9,15}$', message="Phone number must be entered in the local format: '0XXXXXXXX'.")
+	phone_number = models.CharField(validators=[phone_regex], max_length=17, unique=True)
 
+	def __str__(self):
+		return self.name
 
 
 	
